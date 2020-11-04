967f7e1aeffab343300770cd63dbf88a970cf868
Arco Dielhof <arcodielhof@hotmail.com>
2020-11-04 14:51:48 +0100

    AutoUpload test2

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, AIU = ...
 
-local AZPIUInstanceLeadingVersion = 9
+local AZPIUInstanceLeadingVersion = 10
 local dash = " - "
 local name = "InstanceUtility" .. dash .. "InstanceLeading"
 local nameFull = ("AzerPUG " .. name)
